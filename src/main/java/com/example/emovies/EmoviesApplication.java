6c65282caafb4156238b996f2d10c39067aa5eb9
Swapnil Dhavale <swapnildhavale@Swapnils-MacBook-Air.local>
2024-08-15 08:08:35 -0700

    initial starter

---
+++ src/main/java/com/example/emovies/EmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.emovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emovies/EmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
